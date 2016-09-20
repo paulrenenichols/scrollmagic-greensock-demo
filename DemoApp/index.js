38a2e9697b06144389a299bd1e720831bd060276
Paul Rene Nichols <paul.rene.nichols@gmail.com>
2016-09-20 13:46:03 -0500

    first demo improvements
    * smoothed out the animation on this first demo

--- DemoApp/index.js
+++ DemoApp/index.js
@@ -20,39 +20,45 @@ $(document).ready(function () {
 
   // var timeline = new TimelineMax();
 
-  var tween1 = TweenMax.staggerFromTo('#red-scroll-block', 3, {
+  var tween1 = TweenMax.staggerFromTo('#red-scroll-block div', .5, {
       backgroundColor: 'white',
-      rotation: 0
+      rotation: 0,
+      scale: 0.2
     },
     {
       backgroundColor: '#ffaaaa',
-      rotation: 360
+      rotation: 360,
+      scale: 1
     });
 
-  var tween2 = TweenMax.staggerFromTo('#green-scroll-block', 3, {
+  var tween2 = TweenMax.staggerFromTo('#green-scroll-block div', .5, {
       backgroundColor: 'white',
-      rotation: 0
+      rotation: 0,
+      scale: 0.2
     },
     {
       backgroundColor: '#aaffaa',
-      rotation: 360
+      rotation: 360,
+      scale: 1
     });
 
 
-  var tween3 = TweenMax.staggerFromTo('#blue-scroll-block', 3, {
+  var tween3 = TweenMax.staggerFromTo('#blue-scroll-block div', .5, {
       backgroundColor: 'white',
-      rotation: 0
+      rotation: 0,
+      scale: 0.2
     },
     {
       backgroundColor: '#aaaaff',
-      rotation: 360
+      rotation: 360,
+      scale: 1
     });
 
   // timeline.add([tween1, tween2, tween3]);
 
   var scene1 = new Scene({
       triggerElement: '#red-scroll-block',
-      duration: 1000
+      offset: 125
     })
     .setClassToggle('body', 'red') // add class toggle
     .setTween(tween1)
@@ -60,7 +66,7 @@ $(document).ready(function () {
 
   var scene2 = new Scene({
       triggerElement: '#green-scroll-block',
-      duration: 1000
+      offset: 125
     })
     .setClassToggle('body', 'green') // add class toggle
     .setTween(tween2)
@@ -68,7 +74,7 @@ $(document).ready(function () {
 
   var scene3 = new Scene({
       triggerElement: '#blue-scroll-block',
-      duration: 1000
+      offset: 125
     })
     .setClassToggle('body', 'blue') // add class toggle
     .setTween(tween3)
